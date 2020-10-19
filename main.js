474b2d040346aca2c5a66168205f5b08e96ac010
Jaap Kanbier <jaapkanbier@hotmail.com>
2020-10-19 20:03:30 +0200

    added countdown

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-const main = () => {
-    console.log('test');
+"use strict";
+
+const runTimer = () => {
+    var countDownDate = new Date("August 20, 2021 20:00");
+    updateCountDown(countDownDate);
+    var runningInterval = setInterval(updateCountDown, 1000, countDownDate);
 }
 
-window.onload = main
+const updateCountDown = (countDownDate) => {
+    var difference = countDownDate.getTime() - new Date().getTime();
+    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    hours < 10 ? hours = `0${hours}` : hours;
+    minutes < 10 ? minutes = `0${minutes}` : minutes;
+    seconds < 10 ? seconds = `0${seconds}` : seconds;
+    if (difference >= 0 && days == 0)
+    {
+        document.getElementById("countdown").innerHTML = `Today is the day! <br> We are getting married in ${hours}:${minutes}:${seconds}`;
+    }
+    else if (difference < 0)
+    {
+        document.getElementById("countdown").innerHTML = "We are married!! YIPPIE";
+    }
+    else
+    {
+        document.getElementById("countdown").innerHTML = `${days} days & ${hours}:${minutes}:${seconds} hours until we say 'I DO'!`;
+    }
+}
+
+window.onload = runTimer
